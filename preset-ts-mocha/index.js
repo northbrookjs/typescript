c205c2858a9700222ade8a771ec5c06b6485e464
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-08-20 02:35:04 -0400

    feat(preset-ts-mocha): A preset for easier installation of TypeScript plugins
    BREAKING CHANGE: initial implementation

---
+++ preset-ts-mocha/index.js
@@ -0,0 +1,13 @@
+const { plugin: build } = require('northbrook-ts-build')
+const { plugin: create } = require('northbrook-ts-create')
+const { plugin: init } = require('northbrook-ts-init')
+const { plugin: lint } = require('northbrook-tslint')
+const { plugin: mocha } = require('northbrook-ts-mocha')
+
+exports.plugin = function (program, config, workingDir) {
+  build(program, config, workingDir)
+  create(program, config, workingDir)
+  init(program, config, workingDir)
+  lint(program, config, workingDir)
+  mocha(program, config, workingDir)
+}
