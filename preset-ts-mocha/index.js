74bee8004ab15556f06baf39766c194aa46b87fe
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-08-26 10:57:38 -0400

    fix(preset-ts-mocha): fix imports

--- preset-ts-mocha/index.js
+++ preset-ts-mocha/index.js
@@ -1,8 +1,8 @@
-const { plugin: build } = require('northbrook-ts-build')
-const { plugin: create } = require('northbrook-ts-create')
-const { plugin: init } = require('northbrook-ts-init')
-const { plugin: lint } = require('northbrook-tslint')
-const { plugin: mocha } = require('northbrook-ts-mocha')
+const { plugin: build } = require('@northbrook/ts-build')
+const { plugin: create } = require('@northbrook/ts-create')
+const { plugin: init } = require('@northbrook/ts-init')
+const { plugin: lint } = require('@northbrook/tslint')
+const { plugin: mocha } = require('@northbrook/ts-mocha')
 
 exports.plugin = function (program, config, workingDir) {
   build(program, config, workingDir)
