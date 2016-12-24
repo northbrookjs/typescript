68311ca6c51048ace77e8505165fd5bcacc7c635
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-12-23 23:44:02 -0500

    chore(META): add and use tslint plugin

--- northbrook.ts
+++ northbrook.ts
@@ -1,6 +1,7 @@
 import { NorthbrookConfig } from 'northbrook';
 import { plugin as defaultPlugins } from 'northbrook/plugins';
 import { plugin as mocha } from '@northbrook/mocha';
+import { plugin as tslint } from './packages/tslint/src';
 
 const config: NorthbrookConfig =
   {
@@ -8,7 +9,16 @@ const config: NorthbrookConfig =
     plugins: [
       defaultPlugins,
       mocha,
+      tslint,
     ],
-  }
+
+    tslint: {
+      patterns: [
+        /\.ts/,
+        '!**/__test__/**.ts',
+        '!node_modules/**/*.*',
+      ],
+    },
+  };
 
 export = config;
