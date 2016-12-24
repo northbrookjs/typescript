d14001bd54eb2de85aec6258367557e8abe5ea72
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-12-24 15:31:25 -0500

    chore(northbrook): use tslint default config

--- northbrook.ts
+++ northbrook.ts
@@ -1,24 +1,17 @@
-import { NorthbrookConfig } from 'northbrook';
 import { plugin as defaultPlugins } from 'northbrook/plugins';
 import { plugin as mocha } from '@northbrook/mocha';
-import { plugin as tslint } from './packages/tslint/src';
+import { plugin as tslint } from './packages/tslint/src/';
+import { NorthbrookConfig } from 'northbrook';
 
 const config: NorthbrookConfig =
   {
     packages: ['packages/**'],
+
     plugins: [
       defaultPlugins,
       mocha,
       tslint,
     ],
-
-    tslint: {
-      patterns: [
-        /\.ts/,
-        '!**/__test__/**.ts',
-        '!node_modules/**/*.*',
-      ],
-    },
   };
 
 export = config;
