64f9d0f72a30b0db83d1a2a531f2533e3050c616
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-12-24 18:17:13 -0500

    chore(META): add tsc to plugins

--- northbrook.ts
+++ northbrook.ts
@@ -1,6 +1,7 @@
 import { plugin as defaultPlugins } from 'northbrook/plugins';
 import { plugin as mocha } from '@northbrook/mocha';
-import { plugin as tslint } from './packages/tslint/src/';
+import { plugin as tslint } from './packages/tslint/src';
+import { plugin as tsc } from './packages/tsc/src';
 import { NorthbrookConfig } from 'northbrook';
 
 const config: NorthbrookConfig =
@@ -11,7 +12,12 @@ const config: NorthbrookConfig =
       defaultPlugins,
       mocha,
       tslint,
+      tsc,
     ],
+
+    tsc: {
+      es2015: false,
+    },
   };
 
 export = config;
