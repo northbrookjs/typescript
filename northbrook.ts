c3084c4d10ffc3834cebd44db5f674c7ef9e5b2a
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-12-24 19:22:03 -0500

    chore(northbrook): use strings to speed up execution
    ts-node slows things down if there are a lot of imports that are typescript

--- northbrook.ts
+++ northbrook.ts
@@ -1,23 +1,13 @@
-import { plugin as defaultPlugins } from 'northbrook/plugins';
-import { plugin as mocha } from '@northbrook/mocha';
-import { plugin as tslint } from './packages/tslint/src';
-import { plugin as tsc } from './packages/tsc/src';
-import { NorthbrookConfig } from 'northbrook';
-
-const config: NorthbrookConfig =
+const config =
   {
     packages: ['packages/**'],
 
     plugins: [
-      defaultPlugins,
-      mocha,
-      tslint,
-      tsc,
+      'northbrook/plugins',
+      'mocha',
+      './packages/tsc/src',
+      './packages/tslint/src',
     ],
-
-    tsc: {
-      es2015: false,
-    },
   };
 
 export = config;
