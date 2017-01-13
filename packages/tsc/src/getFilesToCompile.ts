0d5906869c8d72f21c3beca25d3d94fb9e32e440
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-01-13 06:32:22 -0500

    fix(tsc): tighten up default pattern matching
    Only compile files in src/ folder per default
    
    AFFECTS: @northbrook/tsc

--- packages/tsc/src/getFilesToCompile.ts
+++ packages/tsc/src/getFilesToCompile.ts
@@ -1,18 +1,19 @@
-import { join } from 'path';
-import { NorthbrookConfig } from 'northbrook';
 import * as expand from 'glob-expand';
+
+import { NorthbrookConfig } from 'northbrook';
+import { join } from 'path';
 
 const defaultPatterns: Array<RegExp | string> =
   [
-    /\.ts/,
-    '!**/__test__/**/*.*',
-    '!lib/**/*.*',
-    '!lib/**/*.*',
-    '!**/*.spec.ts',
-    '!**/*.test.ts',
-    '!**/*Spec.ts',
-    '!**/*Test.ts',
-    '!**/*.skip.ts',
+    'src/**/*.ts',
+    'src/**/*.tsx',
+    'src/**/*.js',
+    '!src/__test__/**/*.*',
+    '!src/**/*.spec.ts',
+    '!src/**/*.test.ts',
+    '!src/**/*Spec.ts',
+    '!src/**/*Test.ts',
+    '!src/**/*.skip.ts',
   ];
 
 export function getFilesToCompile(directory: string, config: NorthbrookConfig) {
